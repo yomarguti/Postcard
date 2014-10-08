981e32e65fc17575841b453d6d7aab9538c5d0de
Yomar Gutierrez <yomargutierrezmejia@iMac-de-Yomar.local>
2014-10-08 12:38:55 -0500

    Primera Version Postcard

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBAction func accionButton(sender: UIButton) {
+        
+        nameLabel.hidden = 0;
+        nameLabel.text = nameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        nameTextField.text = ""
+        nameTextField.resignFirstResponder()
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
